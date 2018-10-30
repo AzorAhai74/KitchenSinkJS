e32d7b77a163821cb632d38b8bdf21bb4973c585
Byron Baldwin <byronsbaldwin@gmail.com>
2018-10-30 15:22:36 -0500

    Updating lab work

---
+++ app.js
@@ -0,0 +1,37 @@
+var firstName = 'Byron';
+var lastName = 'Baldwin';
+
+console.log(firstName);
+console.log(lastName);
+
+const states = 50;
+
+var val1 = 5;
+var val2 = 4;
+
+var result = val1 + val2;
+
+console.log(result);
+
+var firstName = 'Byron';
+var index = 0;
+
+//ASCII Character Code for the letter 'B', at index '0' is 66
+if(firstName.charCodeAt(index) >= 76) {
+    console.log('Back of the Line!');
+} else {
+    console.log('Next!');
+}
+//The condition states that if the first letter of 'Byron' is after the letter 'L'
+//Then it will print 'Back of the Line!'
+//if it is not, then it will print, 'Next!'
+
+console.log(sayHello('Hello World!'))
+
+function sayHello(string) {
+    return(string);
+}
+
+
+
+
